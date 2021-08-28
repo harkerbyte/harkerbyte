fd07df7461c89efb1e274ae04db6f3b07f4b6fa5
shade234sherif <79071277+shade234sherif@users.noreply.github.com>
2021-08-28 07:42:27 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,12 +1,26 @@
-const csvStringToArray = (data) => {
-  const re = /(,|\r?\n|\r|^)(?:"([^"]*(?:""[^"]*)*)"|([^,\r\n]*))/gi
-  const result = [[]]
-  let matches
-  while ((matches = re.exec(data))) {
-    if (matches[1].length && matches[1] !== ',') result.push([])
-    result[result.length - 1].push(
-      matches[2] !== undefined ? matches[2].replace(/""/g, '"') : matches[3]
-    )
-  }
-  return result
-}
+const Telegraf = require('telegraf');
+
+const bot = new Telegraf('1969279863:AAF-4vEZjTW-j2sGyYyyOLuYDaO2NoFqph0');
+
+import csv, io
+
+test_data = [[1, 2, 3], ["please", "follow", "me"]]
+
+# csv module can write data in io.StringIO buffer only
+s = io.StringIO()
+csv.writer(s).writerows(test_data)
+s.seek(0)
+
+# python-telegram-bot library can send files only from io.BytesIO buffer
+# we need to convert StringIO to BytesIO
+buf = io.BytesIO()
+
+# extract csv-string, convert it to bytes and write to buffer
+buf.write(s.getvalue().encode())
+buf.seek(0)
+
+# set a filename with file's extension
+buf.name = f'secret_report_for_cool_guys.csv'
+
+# send the buffer as a regular file
+context.bot.send_document(chat_id=update.message.chat_id, document=buf)
